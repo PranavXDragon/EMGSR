9bea950db4f2a3ff4a7353a9d32d30186a07bec6
unknown <pranavnavghare46@gmail.com>
2026-02-08 01:48:29 +0530

    fix: explicitly pass env vars through next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  },
 }
 
 module.exports = nextConfig
